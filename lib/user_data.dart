1e04cd3173e220d669a6f2fda198f87a760bd56d
Rasmus Eliasson <rasmuseliasson@MacBook-Pro-som-tillhor-Rasmus.local>
2023-09-18 22:21:47 +0200

    boilerplate setup

---
+++ lib/user_data.dart
@@ -0,0 +1,12 @@
+class UserData {
+  final int height;
+  final int weight;
+  final String gender;
+
+  UserData({
+    required this.height,
+    required this.weight,
+    required this.gender,
+  });
+}
+
