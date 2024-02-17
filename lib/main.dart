49e6b6655c366d62398668c500d871378a25ad9e
Rasmus3liasson <ras.muse@hotmail.com>
2024-02-17 15:37:06 +0100

    Add flutter_svg dependency and create SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:alcohol_check/pages/person_details/person_details.dart';
+import 'package:alcohol_check/pages/splash/splash_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// Main function
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) {
-    print('Firebase initialized successfully!');
-  });
-  FirebaseFirestore.instance.settings = Settings(persistenceEnabled: true);
+  await Firebase.initializeApp();
+  FirebaseFirestore.instance.settings =
+      const Settings(persistenceEnabled: true);
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
+// MyApp class
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -29,15 +29,24 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'User Information',
       debugShowCheckedModeBanner: false,
-      home: PersonDeatils(),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (context) => const SplashScreen(),
+        '/details': (context) => const PersonDeatils(),
+      },
       darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.dark,
       theme: ThemeData(
         textTheme: GoogleFonts.montserratTextTheme(
           Theme.of(context).textTheme,
         ),
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: <TargetPlatform, PageTransitionsBuilder>{
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+            TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+          },
+        ),
       ),
     );
   }
 }
-
---
+++ lib/pages/splash/splash_screen.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:alcohol_check/utils/constans/color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _showLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(milliseconds: 1500), () {
+      setState(() {
+        _showLoading = true;
+      });
+      Timer(const Duration(seconds: 2), () {
+        Navigator.of(context).pushReplacementNamed('/details');
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: AppColor.blackColor,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    'Alcohol Check',
+                    style: TextStyle(
+                      color: AppColor.whiteColor,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(width: 10.0),
+                  SvgPicture.asset(
+                    'assets/images/beer.svg',
+                    height: 50.0,
+                    width: 50.0,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 60.0),
+              if (_showLoading)
+                const CircularProgressIndicator(
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColor.whiteColor),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
